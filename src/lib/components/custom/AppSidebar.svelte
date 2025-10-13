4e5a59182ddebcc6bf804214329179cb565607b6
unknown <didadiamsyah02@gmail.com>
2025-10-13 14:21:10 +0700

    fix content

--- src/lib/components/custom/AppSidebar.svelte
+++ src/lib/components/custom/AppSidebar.svelte
@@ -61,18 +61,18 @@
 </script>
 
 <section
-  class="w-full lg:w-[22%] xl:w-[22%] min-h-screen z-50 fixed bg-white shadow-lg slide-in-left flex flex-col justify-between"
+  class="w-full lg:w-[22%] xl:w-[22%] min-h-screen fixed top-0 left-0 z-[9999] bg-white shadow-lg flex flex-col justify-between overflow-y-auto"
 >
-  <div class="flex flex-col overflow-y-auto scrollbar-hide pb-10">
+  <div class="flex flex-col pb-10">
     <div class="flex flex-row items-center p-2 relative">
       <img
         src="https://www.dreamers.id/img_artikel/9yyy.jpg"
         alt="Logo"
-        class="w-full h-[150px] lg:h-full xl:h-full object-cover lg:object-contain xl:object-contain"
+        class="w-full h-[150px] object-cover lg:object-contain xl:object-contain"
         loading="lazy"
       />
       <div
-        class="block lg:hidden xl:hidden absolute top-2 right-2 cursor-pointer bg-white p-1 rounded-full"
+        class="block lg:hidden xl:hidden absolute top-2 right-2 cursor-pointer bg-white p-1 rounded-full z-[10000]"
       >
         <X size={12} onclick={() => sidebar.update((value) => !value)} />
       </div>
@@ -83,13 +83,12 @@
         onclick={() => handleRedirect(item)}
       >
         <item.icon size={20} />
-        <div class="text-sm ml-2">
-          {item.title}
-        </div>
+        <div class="text-sm ml-2">{item.title}</div>
       </button>
     {/each}
   </div>
-  <div class="w-full">
+
+  <div class="w-full border-t border-gray-200">
     <button
       class="w-full flex flex-row items-center bg-white py-2 px-3 hover:bg-blue-800/80 hover:text-white cursor-pointer"
       onclick={() => handleRedirect({ title: "Login", url: "/login" })}
